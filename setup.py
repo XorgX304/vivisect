53c53bb995125c5eb58f115ed9509d64b13681b9
Willi Ballenthin <willi.ballenthin@gmail.com>
2020-06-11 11:14:26 -0600

    setup: bump version to v0.0.20200611-1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='vivisect',
     author='',
     author_email='',
-    version='0.0.20200225',
+    version='v0.0.20200611-1',
     url='https://github.com/vivisect/vivisect',
     py_modules=[''],
     packages=find_packages(),
